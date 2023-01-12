1c53994f352a5c0a153358dfc0012798ce5aef6b
seongddiyong <kodd1102@naver.com>
2023-01-12 23:41:47 +0900

    회원가입 구현 미완성

---
+++ src/main/java/site/lghtsg/api/member/model/PostUserReq.java
@@ -0,0 +1,16 @@
+package site.lghtsg.api.member.model;
+
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class PostUserReq {
+    private String userName;
+    private String email;
+    private int emailCheck;
+    private String password;
+    private String profileImg;
+    private int termsCheck;
+}
