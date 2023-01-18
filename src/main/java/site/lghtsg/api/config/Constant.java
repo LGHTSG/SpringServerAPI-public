880c0fa4ace892d907a333a33f0328a972c11100
Hwichan Jeon <gnlcks82@gmail.com>
2023-01-18 09:03:04 +0900

    [FIX](InProcess) : conflict solved, fixing unBuildable code

--- src/main/java/site/lghtsg/api/config/Constant.java
+++ src/main/java/site/lghtsg/api/config/Constant.java
@@ -4,10 +4,7 @@ package site.lghtsg.api.config;
 public class Constant {
     // 예시 public static final String IP_ADDRESS = "127.0.0.1";
     public static int PAGE_CONTENT_LIMIT = 2;
-<<<<<<< HEAD
-=======
 
->>>>>>> resellMerge
     public static String ASCENDING_PARAM = "ascending";
     public static String DESCENDING_PARAM = "descending";
     public static String SORT_FLUCTUATION_PARAM = "fluctuation";
