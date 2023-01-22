291260d5d5f531027f89f4c12f39a128dbe7a259
박준영 <wnsdud6969@naver.com>
2023-01-22 22:27:49 +0900

    [Test] 테스트 코드 수정

--- src/test/java/site/lghtsg/api/resells/StartScrapping.java
+++ src/test/java/site/lghtsg/api/resells/StartScrapping.java
@@ -3,12 +3,9 @@ package site.lghtsg.api.resells;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.stereotype.Service;
-import site.lghtsg.api.resells.model.GetResellRes;
+import site.lghtsg.api.resells.model.GetResellBoxRes;
 
 import java.util.List;
-
-import static site.lghtsg.api.config.Constant.ASCENDING_PARAM;
 
 @SpringBootTest
 public class StartScrapping {
@@ -21,10 +18,6 @@ public class StartScrapping {
     }
     @Test
     void 리스트(){
-        List<GetResellRes> resellResList = resellDao.getResellsByRate(ASCENDING_PARAM);
-    }
-    @Test
-    void 스크래핑2(){
-//        resellDao.scrapingTest();
+        List<GetResellBoxRes> resellResList = resellDao.getResellBoxes();
     }
 }
