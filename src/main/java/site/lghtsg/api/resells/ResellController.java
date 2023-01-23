93040e7acdc936fb521989df6f0008bcb5b2647c
박준영 <wnsdud6969@naver.com>
2023-01-23 22:01:00 +0900

    [Refactor] ResellController의 resellIdx(int -> long) 및 sort, order의 null값 처리

--- src/main/java/site/lghtsg/api/resells/ResellController.java
+++ src/main/java/site/lghtsg/api/resells/ResellController.java
@@ -12,37 +12,29 @@ import site.lghtsg.api.resells.model.GetResellBoxRes;
 
 import java.util.List;
 
+import static site.lghtsg.api.config.Constant.PARAM_DEFAULT;
+
 @RestController
 @RequestMapping("/resells")
 public class ResellController {
     final Logger logger = LoggerFactory.getLogger(this.getClass());
-
     @Autowired
     private final ResellProvider resellProvider;
 
-    @Autowired
-    private final ResellDao resellDao;
-
-    @Autowired
-    private final ResellService resellService;
-
-
-    public ResellController(ResellProvider resellProvider, ResellDao resellDao, ResellService resellService) {
+    public ResellController(ResellProvider resellProvider) {
         this.resellProvider = resellProvider;
-        this.resellDao = resellDao;
-        this.resellService = resellService;
     }
 
     @ResponseBody
     @GetMapping("")
     public BaseResponse<List<GetResellBoxRes>> getResellBoxes(@RequestParam(required = false) String sort, @RequestParam(required = false) String order) {
-        try {
-            if (sort == null) {
-                List<GetResellBoxRes> getGetResellBoxesRes = resellProvider.getResellBoxesByIdx(order);
-                return new BaseResponse<>(getGetResellBoxesRes);
-            }
+        if (sort == null)
+            sort = PARAM_DEFAULT;
+        if (order == null)
+            order = PARAM_DEFAULT;
 
-            List<GetResellBoxRes> getGetResellBoxesRes = resellProvider.getResellsByRate(order);
+        try {
+            List<GetResellBoxRes> getGetResellBoxesRes = resellProvider.getResellBoxes(sort, order);
             return new BaseResponse<>(getGetResellBoxesRes);
         } catch (BaseException e) {
             return new BaseResponse<>((e.getStatus()));
@@ -50,14 +42,8 @@ public class ResellController {
     }
 
     @ResponseBody
-    @GetMapping("/test")
-    public void test(){
-        //resellDao.scraping();
-    }
-
-    @ResponseBody
     @GetMapping("/{resellIdx}/info")
-    public BaseResponse<GetResellInfoRes> getResellInfo(@PathVariable("resellIdx") int resellIdx) {
+    public BaseResponse<GetResellInfoRes> getResellInfo(@PathVariable("resellIdx") long resellIdx) {
         try {
             GetResellInfoRes getResellInfoRes = resellProvider.getResellInfo(resellIdx);
             return new BaseResponse<>(getResellInfoRes);
@@ -69,7 +55,7 @@ public class ResellController {
 
     @ResponseBody
     @GetMapping("/{resellIdx}/prices")
-    public BaseResponse<List<GetResellTransactionRes>> getResellTransaction(@PathVariable("resellIdx") int resellIdx) {
+    public BaseResponse<List<GetResellTransactionRes>> getResellTransaction(@PathVariable("resellIdx") long resellIdx) {
         try {
             List<GetResellTransactionRes> getResellTransactionRes = resellProvider.getResellTransaction(resellIdx);
             return new BaseResponse<>(getResellTransactionRes);
