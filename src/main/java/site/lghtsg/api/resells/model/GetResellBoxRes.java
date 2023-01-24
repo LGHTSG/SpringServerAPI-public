9450394e632db743787d26b8a1cdaf892299925c
박준영 <wnsdud6969@naver.com>
2023-01-24 17:40:52 +0900

    [Refactor] lastPrice 추가

--- src/main/java/site/lghtsg/api/resells/model/GetResellBoxRes.java
+++ src/main/java/site/lghtsg/api/resells/model/GetResellBoxRes.java
@@ -1,7 +1,11 @@
 package site.lghtsg.api.resells.model;
 
+import lombok.Getter;
+import lombok.Setter;
 import site.lghtsg.api.common.model.Box;
 
+@Getter
+@Setter
 public class GetResellBoxRes extends Box{
-
+    private Long lastPrice;
 }
