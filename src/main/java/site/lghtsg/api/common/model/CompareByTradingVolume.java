d3bfe6fa004336e9d9f0427d8416077597a78939
김현진 <hikhjin@gmail.com>
2023-01-25 19:07:33 +0900

    코드 수정

--- src/main/java/site/lghtsg/api/common/model/CompareByTradingVolume.java
+++ src/main/java/site/lghtsg/api/common/model/CompareByTradingVolume.java
@@ -1,16 +1,9 @@
 package site.lghtsg.api.common.model;
 
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-import site.lghtsg.api.common.model.Box;
 import site.lghtsg.api.stocks.model.StockBox;
 
 import java.util.Comparator;
 
-@Getter
-@Setter
-@NoArgsConstructor
 public class CompareByTradingVolume implements Comparator<StockBox> {
     public int compare(StockBox o1, StockBox o2) {
         double o1MarketCap = o1.getTradingVolume(), o2MarketCap = o2.getTradingVolume();
