18d6039f1e1540f299b38a9258cf93127053aeeb
박준영 <wnsdud6969@naver.com>
2023-01-26 16:38:22 +0900

    [Feat] dataUploader 패키지 및 Resell 클래스 생성

---
+++ src/main/java/site/lghtsg/api/resells/dataUploader/Resell.java
@@ -0,0 +1,19 @@
+package site.lghtsg.api.resells.dataUploader;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class Resell {
+    private String name;
+    private String releasedPrice;
+    private String releasedDate;
+    private String color;
+    private String brand;
+    private String productNum;
+    private String image1;
+    private Long iconImageIdx;
+}
