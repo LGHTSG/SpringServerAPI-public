a3198a58e76e02bf6265d1be1ca1596ea0c896d5
박준영 <wnsdud6969@naver.com>
2023-01-26 16:45:10 +0900

    [Feat] ResellUploadDao 구현

--- src/main/java/site/lghtsg/api/resells/dataUploader/ResellUploadDao.java
+++ src/main/java/site/lghtsg/api/resells/dataUploader/ResellUploadDao.java
@@ -1,4 +1,50 @@
 package site.lghtsg.api.resells.dataUploader;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+import site.lghtsg.api.resells.dataUploader.model.Resell;
+import site.lghtsg.api.resells.dataUploader.model.ResellTransaction;
+
+import javax.sql.DataSource;
+
+@Repository
 public class ResellUploadDao {
+
+    private JdbcTemplate jdbcTemplate;
+
+    @Autowired
+    public void setDataSource(DataSource dataSource) {
+        this.jdbcTemplate = new JdbcTemplate(dataSource);
+    }
+
+    public void uploadResell(Resell resell) {
+        String createResellQuery = "insert into Resell (name, releasedPrice, releasedDate, color, brand, productNum, image1, iconImageIdx) VALUES (?,?,?,?,?,?,?,?)";// 실행될 동적 쿼리문
+
+        Object[] createResellParams = new Object[]{
+                resell.getName(),
+                resell.getReleasedPrice(),
+                resell.getReleasedDate(),
+                resell.getColor(),
+                resell.getBrand(),
+                resell.getProductNum(),
+                resell.getImage1(),
+                resell.getIconImageIdx()
+        };
+
+        this.jdbcTemplate.update(createResellQuery, createResellParams);
+    }
+
+    public void uploadResellTransaction(ResellTransaction resellTransaction) {
+        String createResellQuery = "insert into ResellTransaction (resellIdx, price, transactionTime) VALUES(?,?,?)"; // 실행될 동적 쿼리문
+
+        Object[] createResellTransactionParams = new Object[]{
+                resellTransaction.getResellIdx(),
+                resellTransaction.getPrice(),
+                resellTransaction.getTransactionTime()
+        };
+
+
+        this.jdbcTemplate.update(createResellQuery, createResellTransactionParams);
+    }
 }
